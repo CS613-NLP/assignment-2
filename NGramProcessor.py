cbff500890e60c3bc2006b386e31d17e546964c2
Kanishk-py <kanishk.singhal@iitgn.ac.in>
2023-09-16 02:01:56 +0530

    NGram

--- NGramProcessor.py
+++ NGramProcessor.py
@@ -1,10 +1,14 @@
 import pandas as pd
+import numpy as np
+from collections import defaultdict
+from tqdm import tqdm 
+import re
 
 class NGramProcessor:
     """
     This class is used to create n-gram language models
     """    
-    def __init__(self, df_train, df_test, ngram=1):
+    def __init__(self, df_train, df_test, n=1):
         """
 
         Args:
@@ -14,77 +18,137 @@ class NGramProcessor:
         """        
         self.df_train = df_train
         self.df_test = df_test
-        self.ngram = ngram
+        self.n = n
         pass
 
-    def ngrams(self, df, ngram=1):
+    def make_sentences(self, comment):
+        """Function to create a sentence from a comment
+
+        Args:
+            comment (str): Comment to create a sentence from
+
+        Returns:
+            list: list of sentences
+        """        
+        pattern = r'<s> <s> <s>(.*?)</s> </s> </s>'
+        sentences = re.findall(pattern, comment, re.DOTALL)
+        sentences = ['<s> <s> <s> ' + sentence.strip() + ' </s> </s> </s>' for sentence in sentences]
+        return sentences
+
+    def ngrams(self, df, n=1, startIdx=0):
         """Function to create n-grams from a dataset
 
         Args:
             df (pd.DataFrame): Preprocessed dataset to create n-grams from
-            ngram (int, optional): Value of n for the n-gram. Defaults to 1.
+            n (int, optional): Value of n for the n-gram. Defaults to 1.
 
         Returns:
             list: list of n-grams
         """        
         ngrams_corpus = []
-        for _, row in df.iterrows():
-            words = row['Processed_Comment'].split(" ")
-            ngrams_sentence = [' '.join(words[i:i + ngram]) for i in range(len(words) - ngram + 1)]
-            ngrams_corpus.extend(ngrams_sentence)
+        for _, row in tqdm(df.iterrows(), total=df.shape[0], desc=f'Creating {n}-grams'):
+            sentences = self.make_sentences(row['Processed_Comment'])
+            for sentence in sentences:
+                words = sentence.split(" ")
+                ngrams_sentence = [' '.join(words[i:i + n]) for i in range(startIdx, len(words) - n - 1)]
+                ngrams_corpus.extend(ngrams_sentence)
         return ngrams_corpus
 
-    def create_frequency_dict(self, ngram):
+    def create_frequency_dict(self, n, startIdx=0):
         """Function to create a frequency dictionary for n-grams
 
         Args:
-            ngram (int): Value of n for the n-gram
+            n (int): Value of n for the n-gram
 
         Returns:
             Dict: Dictionary of n-grams and their frequencies
         """        
-        ngram = self.ngrams(self.df_train, ngram)
-        frequency_dict = {}
-        for item in ngram:
-            if item in frequency_dict:
-                frequency_dict[item] += 1
-            else:
-                frequency_dict[item] = 1
+        ngrams = self.ngrams(self.df_train, n, startIdx)
+        frequency_dict = defaultdict(int)
+        
+        for item in tqdm(ngrams, desc=f'Creating frequency dictionary for {n}-grams'):
+            frequency_dict[item] += 1
+            # if item in frequency_dict:
+            #     frequency_dict[item] += 1
+            # else:
+            #     frequency_dict[item] = 1
         return frequency_dict
 
-    def find_probability(self, train=True, save_csv=''):
+    def find_probability(self, save_csv='sample.csv'):
         """Function to find the probability of n-grams
 
         Args:
-            train (bool, optional): Boolean value to indicate if the function is being called for 
-                                    training or validation. Defaults to True.
-            save_csv (str, optional): Path to save the csv file. Defaults to ''.
+            save_csv (str, optional): Path to save the csv file. Defaults to 'sample.csv'.
 
         Returns:
             pd.DataFrame: Dataframe containing the n-grams and their probabilities
         """        
-        if(self.ngram != 1):
-            frequency_dict_n_1 = self.create_frequency_dict(self.ngram - 1)
-        frequency_dict = self.create_frequency_dict(self.ngram)
+        if(self.n != 1):
+            frequency_dict_n_1 = self.create_frequency_dict(self.n-1, 4-self.n)
+        frequency_dict = self.create_frequency_dict(self.n, 4-self.n)
         probability_dict = {}
-        if train:
-            for key in frequency_dict.keys():
-                if self.ngram == 1:
-                    probability_dict[key] = (frequency_dict[key] if bool(frequency_dict.get(key)) else 0) / sum(frequency_dict.values())
-                else:
-                    key_n_1 = ' '.join(key.split()[:self.ngram - 1])
-                    probability_dict[key] = (frequency_dict[key] if bool(frequency_dict.get(key)) else 0) / (frequency_dict_n_1[key_n_1] if bool(frequency_dict_n_1.get(key_n_1)) else 0)
-        else:
-            ngram = self.ngrams(self.df_test, self.ngram)
-            for key in list(set(ngram)):
-                if self.ngram == 1:
-                    probability_dict[key] = (frequency_dict[key] if bool(frequency_dict.get(key)) else 0) / sum(frequency_dict.values())
 
+        # if train:
+        for key in tqdm(frequency_dict.keys(), desc=f'Finding probability for {self.n}-grams'):
+            if self.n == 1:
+                probability_dict[key] = frequency_dict[key] / sum(frequency_dict.values())
+            else:
+                key_n_1 = ' '.join(key.split()[:self.n - 1])
+                if bool(frequency_dict_n_1.get(key_n_1)):
+                    probability_dict[key] = frequency_dict[key] / frequency_dict_n_1[key_n_1] 
                 else:
-                    key_n_1 = ' '.join(key.split()[:self.ngram - 1])
-                    probability_dict[key] = (0 if not bool(frequency_dict_n_1.get(key_n_1)) else (frequency_dict[key] if bool(frequency_dict.get(key)) else 0) / frequency_dict_n_1[key_n_1])
+                    probability_dict[key] = 0
 
-        if save_csv != '':
-            df = pd.DataFrame(list(probability_dict.items()), columns=['Phrase', 'Probability'])
-            df.to_csv(save_csv, index=False)
+        df = pd.DataFrame(list(probability_dict.items()), columns=['Comment', 'Probability'])
+        
+        df.to_csv(save_csv, index=False)
+        print(f'Saved {self.n}-gram probabilities to {save_csv}')
+        return df
+
+    
+    def calc_perplexity(self, save_csv='sample.csv'):
+        """Function to find the probability of n-grams
+
+        Args:
+            save_csv (str, optional): Path to save the csv file. Defaults to 'sample.csv'.
+
+        Returns:
+            pd.DataFrame: Dataframe containing the n-grams and their probabilities
+        """
+        
+        if(self.n != 1):
+            frequency_dict_n_1 = self.create_frequency_dict(self.n-1, 4-self.n)
+        frequency_dict = self.create_frequency_dict(self.n, 4-self.n)
+        probability_dict = {}
+
+        ngram = self.ngrams(self.df_test, self.n)
+        for key in list(set(ngram)):
+            if self.n == 1:
+                probability_dict[key] = frequency_dict[key] / sum(frequency_dict.values())
+
+            else:
+                key_n_1 = ' '.join(key.split()[:self.n - 1])
+                if bool(frequency_dict_n_1.get(key_n_1)):
+                    probability_dict[key] = frequency_dict[key] / frequency_dict_n_1[key_n_1]
+                else:
+                    probability_dict[key] = 0
+                # probability_dict[key] = (0 if not bool(frequency_dict_n_1.get(key_n_1)) else (frequency_dict[key] if bool(frequency_dict.get(key)) else 0) / frequency_dict_n_1[key_n_1])
+        
+        df = pd.DataFrame(columns=['Comment', 'Perplexity'])
+        for idx, row in tqdm(self.df_test.iterrows(), total=self.df_test.shape[0], desc=f'Calculating perplexity for {self.n}-grams'):
+            sentences = self.make_sentences(row['Processed_Comment'])
+            perplexity = 0
+            for sentence in sentences:
+                words = sentence.split(" ")
+                ngrams_sentence = [' '.join(words[i:i + self.n]) for i in range(4-self.n, len(words) - self.n - 1)]
+                total_log_prob = 0
+                for ngram_set in ngrams_sentence:
+                    total_log_prob += np.log2(probability_dict[ngram_set])
+                perplexity += 2 ** (-total_log_prob / len(ngrams_sentence))
+            
+            # df.at[idx, 'Perplexity'] = perplexity/len(sentences)
+            df.loc[idx] = [row['Processed_Comment'], perplexity/len(sentences)]
+
+        df.to_csv(save_csv, index=False)
+        print(f'Saved {self.n}-gram perplexities to {save_csv}')
         return df
--- ngrams_train.py
+++ ngrams_train.py
@@ -8,24 +8,20 @@ df_test = pd.read_csv('dataset/validate_dataset.csv')
 # df_test.dropna(inplace=True)
 
 
-unigram = NGramProcessor(df_train, df_test, 1)
-unigram.find_probability(train=True, save_csv='language_models/unigram.csv')
-unigram.find_probability(train=False, save_csv='language_models/unigram_val.csv')
+unigram = NGramProcessor(df_train, df_train, 1)
+unigram.find_probability(save_csv='language_models/unigram.csv')
+unigram.calc_perplexity(save_csv='language_models/unigram_val.csv')
 
 
-bigram = NGramProcessor(df_train, df_test, 2)
-bigram.find_probability(train=True, save_csv='language_models/bigram.csv')
-bigram.find_probability(train=False, save_csv='language_models/bigram_val.csv')
-
+bigram = NGramProcessor(df_train, df_train, 2)
+bigram.find_probability(save_csv='language_models/bigram.csv')
+bigram.calc_perplexity(save_csv='language_models/bigram_val.csv')
 
 trigram = NGramProcessor(df_train, df_test, 3)
-trigram.find_probability(train=True, save_csv='language_models/trigram.csv')
-trigram.find_probability(train=False, save_csv='language_models/trigram_val.csv')
+trigram.find_probability(save_csv='language_models/trigram.csv')
+trigram.find_probability(save_csv='language_models/trigram_val.csv')
 
 
 quadgram = NGramProcessor(df_train, df_test, 4)
-quadgram.find_probability(train=True, save_csv='language_models/quadgram.csv')
-quadgram.find_probability(train=False, save_csv='language_models/quadgram_val.csv')
-
-
-
+quadgram.find_probability(save_csv='language_models/quadgram.csv')
+quadgram.find_probability(save_csv='language_models/quadgram_val.csv')
