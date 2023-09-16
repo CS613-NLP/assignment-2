bf7e30fb492f34385d3901d1f7cee193c2f2f7ff
Joymakwana2003 <97979123+Joymakwana2003@users.noreply.github.com>
2023-09-16 22:18:29 +0530

    added traintestsplit.py

---
+++ dataset/TrainTestSplit.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# splitting the data into train and validate sets in the ratio 80:20
+
+data = pd.read_csv('processed_dataset.csv')
+
+data = data.sample(frac=1, random_state=42)
+
+train_ratio = 0.8
+train_size = int(len(data) * train_ratio)
+
+train_data = data.iloc[:train_size]
+validate_data = data.iloc[train_size:]
+
+train_data.to_csv('train_dataset.csv', index=False)
+validate_data.to_csv('validate_dataset.csv', index=False)
